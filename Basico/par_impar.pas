77dbd71c1c7fa586348ad6d5831f91a43ef28fe6
OswaldjosueDev <figueroaoswald3@gmail.com>
2026-06-22 12:01:33 -0500

    Create par_impar.pas

---
+++ Basico/par_impar.pas
@@ -0,0 +1,23 @@
+Program Par_Impar;
+
+Var
+
+  Numero : Integer;
+
+Begin
+
+  Writeln('Ingresa un numero');
+  Readln(Numero);
+
+  If numero MOD 2 = 0 then
+  Begin
+      Writeln('El numero es par')
+  End
+  Else
+  Begin
+      Writeln('El numero es impar');
+  end;
+
+  readln;
+End.
+
